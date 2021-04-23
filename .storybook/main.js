3bfe246224b5a8a15db8dd290c189fd5bda1f65a
Aaron Ladage <aladage@degdigital.com>
2021-04-23 10:44:18 -0500

    updated config files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,24 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-css-modules-preset'
+  ],
+  webpackFinal: async config => {
+    config.resolve = {
+      alias: {
+        '@components': path.resolve(__dirname, '../components'),
+        '@qraphql': path.resolve(__dirname, '../graphql'),
+        '@hooks': path.resolve(__dirname, '../hooks'),
+        '@pages': path.resolve(__dirname, '../pages'),
+        '@public': path.resolve(__dirname, '../public'),
+        '@styles': path.resolve(__dirname, '../styles'),
+        '@utils': path.resolve(__dirname, '../utils')
+      }
+    };
+    return config;
+  }
 };
